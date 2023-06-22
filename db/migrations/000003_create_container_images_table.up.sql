b28d6431a9aca0a68c5a38f7e221cb4ce7c842bc
John Wregglesworth <wregglej@cyverse.org>
2023-06-22 14:53:58 -0700

    Start adding command to insert images into the database. Have insert and list working

--- db/migrations/000003_create_container_images_table.up.sql
+++ db/migrations/000003_create_container_images_table.up.sql
@@ -2,12 +2,12 @@ USE de_releases;
 
 -- Contains records of all container images needed for a release/deployment.
 CREATE TABLE IF NOT EXISTS container_images (
-    id           INT            NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    repo_id      INT            NOT NULL,
-    dockerfile   TEXT           NOT NULL,
-    registry     VARCHAR(255)   NOT NULL,
-    name         VARCHAR(255)   NOT NULL,
-    tag          VARCHAR(255)   NOT NULL,
+    id           INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    repo_id      INT  NOT NULL,
+    dockerfile   TEXT NOT NULL,
+    name         TEXT NOT NULL,
+    tag          TEXT NOT NULL,
+    digest       TEXT NOT NULL,
 
     FOREIGN KEY (repo_id) REFERENCES repos(id)
 );
---
+++ src/bin/mgmt-container-images/main.rs
@@ -0,0 +1,188 @@
+use anyhow::{anyhow, Result};
+use clap::{arg, Command};
+use sqlx::mysql::{MySql, MySqlPoolOptions};
+use sqlx::{Pool, Transaction};
+
+fn cli() -> Command {
+    Command::new("mgmt-container-images")
+        .about("Manages container images in the de_releases database")
+        .args_conflicts_with_subcommands(true)
+        .subcommand_required(true)
+        .arg(
+            arg!(-d --"database-url" <DATABASE>)
+                .default_value("mysql://root@127.0.0.1:3306/de_releases")
+                .value_parser(clap::value_parser!(String)),
+        )
+        .subcommand(
+            Command::new("insert")
+                .args_conflicts_with_subcommands(true)
+                .arg(arg!(-i --"image" <IMAGE>).value_parser(clap::value_parser!(String)))
+                .arg(arg!(-s --"service" <SERVICE>).value_parser(clap::value_parser!(String)))
+                .arg(arg!(-f --dockerfile <DOCKERFILE>).value_parser(clap::value_parser!(String))),
+        )
+        .subcommand(
+            Command::new("insert-builds")
+                .args_conflicts_with_subcommands(true)
+                .arg(arg!(-b --"builds-dir" <DIR>).value_parser(clap::value_parser!(String))),
+        )
+        .subcommand(
+            Command::new("delete")
+                .args_conflicts_with_subcommands(true)
+                .arg(arg!(-i --"image" <IMAGE>).value_parser(clap::value_parser!(String))),
+        )
+        .subcommand(Command::new("list").args_conflicts_with_subcommands(true))
+}
+
+struct ContainerImageParts {
+    name: String,
+    tag: String,
+    digest: String,
+}
+
+fn parse_container_image(image: &str) -> Result<ContainerImageParts> {
+    let mut parts = image.split('@');
+    let name_and_tag = parts
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("image name not found"))?;
+    let digest = parts
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("digest not found"))?;
+
+    let mut parts = name_and_tag.split(':');
+    let name = parts
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("image name not found"))?;
+    let tag = parts
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("tag not found"))?;
+
+    Ok(ContainerImageParts {
+        name: name.to_string(),
+        tag: tag.to_string(),
+        digest: digest.to_string(),
+    })
+}
+
+async fn get_service_repo_id(tx: &mut Transaction<'_, MySql>, service: &str) -> Result<i32> {
+    let repo_id = sqlx::query!(
+        r#"
+        SELECT repo_id FROM services WHERE name = (?)
+        "#,
+        service,
+    )
+    .fetch_one(tx)
+    .await?
+    .repo_id
+    .ok_or_else(|| anyhow::anyhow!("repo_id not found"))?;
+
+    Ok(repo_id)
+}
+
+async fn insert_image(
+    pool: &Pool<MySql>,
+    service: &str,
+    dockerfile: &str,
+    image: &str,
+) -> Result<()> {
+    let mut tx = pool.begin().await?;
+    let repo_id = get_service_repo_id(&mut tx, service).await?;
+    let container_image = parse_container_image(image)?;
+
+    let image_id = sqlx::query!(
+        r#"
+            INSERT INTO container_images 
+                (name, tag, digest, repo_id, dockerfile)
+            VALUES 
+                (?, ?, ?, ?, ?)
+        "#,
+        container_image.name,
+        container_image.tag,
+        container_image.digest,
+        repo_id,
+        dockerfile,
+    )
+    .execute(&mut tx)
+    .await?
+    .last_insert_id();
+
+    println!("Inserted image with id {}", image_id);
+
+    tx.commit().await?;
+
+    Ok(())
+}
+
+async fn list_images(pool: &Pool<MySql>) -> Result<()> {
+    let images = sqlx::query!(
+        r#"
+        SELECT 
+            ci.id, ci.name, ci.tag, ci.digest, ci.dockerfile, s.name as service_name
+        FROM 
+            container_images ci
+        INNER JOIN
+            services s
+        ON
+            ci.repo_id = s.repo_id
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    println!("Images:");
+    for image in images {
+        println!(
+            "  id: {}, name: {}, tag: {}, digest: {}, dockerfile: {}, service: {}",
+            image.id.ok_or_else(|| anyhow!("missing id"))?,
+            image.name.ok_or_else(|| anyhow!("missing name"))?,
+            image.tag.ok_or_else(|| anyhow!("missing tag"))?,
+            image.digest.ok_or_else(|| anyhow!("missing digest"))?,
+            image
+                .dockerfile
+                .ok_or_else(|| anyhow!("missing dockerfile"))?,
+            image
+                .service_name
+                .ok_or_else(|| anyhow!("missing service_name"))?,
+        );
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let command = cli().get_matches();
+
+    let database_url = command
+        .get_one::<String>("database-url")
+        .unwrap_or_else(|| {
+            panic!("No database URL specified. Use --database-url <url> to specify a database URL.")
+        });
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await?;
+
+    match command.subcommand() {
+        Some(("insert", sub_m)) => {
+            let image = sub_m.get_one::<String>("image").ok_or_else(|| {
+                anyhow!("No image specified. Use --image <image> to specify an image to insert.")
+            })?;
+            let dockerfile = sub_m.get_one::<String>("dockerfile").ok_or_else(|| {
+                anyhow!("No dockerfile specified. Use --dockerfile <dockerfile> to specify a dockerfile to insert.")
+            })?;
+            let service = sub_m.get_one::<String>("service").ok_or_else(|| {
+                anyhow!(
+                    "No service specified. Use --service <service> to specify a service to insert."
+                )
+            })?;
+            insert_image(&pool, &service, &dockerfile, &image).await?;
+        }
+        Some(("list", _)) => {
+            list_images(&pool).await?;
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
+}
