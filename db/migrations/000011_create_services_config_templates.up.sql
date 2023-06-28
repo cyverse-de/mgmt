eb8fa64d698bf2868486f6955892fb2ac3800609
John Wregglesworth <wregglej@cyverse.org>
2023-06-28 14:28:15 -0700

    Renamed service_configs to services_config_templates, removed FK to config_values

--- db/migrations/000011_create_services_config_templates.up.sql
+++ db/migrations/000011_create_services_config_templates.up.sql
@@ -1,14 +1,14 @@
 USE de_releases;
 
 -- Maps services to config templates and config values.
-CREATE TABLE IF NOT EXISTS services_configs (
+CREATE TABLE IF NOT EXISTS services_config_templates (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     service_id INT NOT NULL,
     config_template_id INT NOT NULL,
-    config_value_id INT NOT NULL,
-    path TEXT NOT NULL,
+
+    -- The path to the rendered config file.
+    path TEXT NOT NULL, 
 
     FOREIGN KEY (service_id) REFERENCES services(id),
-    FOREIGN KEY (config_template_id) REFERENCES config_templates(id),
-    FOREIGN KEY (config_value_id) REFERENCES services_config_values(id)
+    FOREIGN KEY (config_template_id) REFERENCES config_templates(id)
 );
