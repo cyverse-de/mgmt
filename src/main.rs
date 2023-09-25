ff30f02a78af32db5335102af8e0b43810fa476f
John Wregglesworth <wregglej@cyverse.org>
2023-09-25 14:48:56 -0700

    Start work on unifying commands

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use clap::Command;
+use anyhow::Result;
+use clap::{arg, Command};
 use mgmt::app;
-use mgmt::cli::{configs, container_images, release, site};
+use mgmt::cli::{configs, container_images, deploy, release, site};
+use mgmt::handlers;
+use sqlx::mysql::MySqlPoolOptions;
 use which::which;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let commands = Command::new("mgmt")
         .version("0.1.0")
         .about("Discovery Environment deployment management tool")
         .args_conflicts_with_subcommands(true)
+        .subcommand_required(true)
+        .arg(
+            arg!(-d --"database-url" <DATABASE>)
+                .default_value("mysql://root@127.0.0.1:3306/de_releases")
+                .value_parser(clap::value_parser!(String)),
+        )
         .subcommand(configs::cli())
         .subcommand(container_images::cli())
         .subcommand(release::cli())
-        .subcommand(site::cli());
-    let cli = commands.get_matches();
+        .subcommand(site::cli())
+        .subcommand(deploy::cli())
+        .get_matches();
 
     let skaffold_path = match which("skaffold") {
         Ok(path_buf) => path_buf,
@@ -42,20 +53,32 @@ fn main() {
     println!("git path: {}", git_path.display());
     println!("kubectl path: {}", kubectl_path.display());
 
-    let state = app::App::from(&cli).unwrap_or_else(|e| {
-        println!("{}", e);
-        std::process::exit(1);
-    });
+    let database_url = commands
+        .get_one::<String>("database-url")
+        .unwrap_or_else(|| {
+            panic!("No database URL specified. Use --database-url <url> to specify a database URL.")
+        });
 
-    match state.process() {
-        Ok(status) => {
-            if !status {
-                println!("failed to deploy");
-                return;
-            }
-        }
-        Err(e) => {
-            println!("{}", e);
-        }
-    }
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await
+        .unwrap_or_else(|e| {
+            println!("error connecting to database: {}", e);
+            std::process::exit(1);
+        });
+
+    // let state = app::App::from(&cli).unwrap_or_else(|e| {
+    //     println!("{}", e);
+    //     std::process::exit(1);
+    // });
+
+    match commands.subcommand() {
+        Some(("configs", sub_m)) => {}
+        Some(("container-images", sub_m)) => {}
+        Some(("release", sub_m)) => {}
+        Some(("site", sub_m)) => {}
+        Some(("deploy", sub_m)) => {}
+        _ => unreachable!(),
+    };
 }
