a43a20fc7b6e9730bb18ac3b243b72186b97a229
John Wregglesworth <wregglej@cyverse.org>
2023-10-17 14:58:58 -0700

    Added comment to a statement in the 000012 migration

--- db/migrations/000012_create_environments_services_config_templates.up.sql
+++ db/migrations/000012_create_environments_services_config_templates.up.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS environments_services_config_templates (
     environment_service_id INT NOT NULL,
     config_template_id INT NOT NULL,
 
-    --- The path to the rendered config file.
+    --- The path to the rendered config file. Should be set, but is not yet used during deployments.
     path TEXT NOT NULL,
 
     FOREIGN KEY (environment_service_id) REFERENCES environments_services(id) ON DELETE CASCADE,
